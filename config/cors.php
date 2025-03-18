eb373a246d284791e382c234311ff33e2bececf2
Janssen Uy <janssen.uy.dev@gmail.com>
2025-03-18 09:43:06 +0800

    refactor: specific allowed origin in cors

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://nc-frontend-haythamac-haythamacs-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
