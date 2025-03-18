d1a90d783c1b44501a89cab5fff9a5ac6b7c50b8
Janssen Uy <janssen.uy.dev@gmail.com>
2025-03-18 09:27:55 +0800

    feat: session controller added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,14 @@ Route::get('/hello', function() {
     return response()->json(['message' => 'Hello World!']);
 });
 
+Route::post('login', [SessionController::class, 'login']); 
 Route::apiResource('players', PlayerController::class);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [SessionController::class, 'logout']); 
+    Route::apiResource('players', PlayerController::class);
+    
+});
